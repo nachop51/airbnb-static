8aa3344aedd79cad6eba20b5ab61d06dcfe8219c
Nachop51 <nachoperalta0@gmail.com>
2022-06-29 13:50:53 +0000

    console and filestorage updated

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Console module """
 import cmd
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -8,12 +9,83 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """Quit command to exit the program"""
+        """Quit command to exit the program
+        """
         return True
 
     def do_EOF(self, arg):
-        """Ends the program"""
+        """Ends the program
+        """
         return True
+
+    def emptyline(self):
+        """In case of an empty line
+        """
+        pass
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it, and prints the id
+        """
+        if arg is None or arg == "":
+            print("** class name missing **")
+        elif arg not in storage.classes():
+            print("** class doesn't exist **")
+        else:
+            obj = storage.classes()[arg]()
+            print(obj.id)
+            obj.save()
+
+    def do_show(self, arg):
+        """Prints the string representation of an
+        instance based on the class name and id
+        """
+        if arg is None or arg == "":
+            print("** class name missing **")
+        else:
+            arg = arg.split()
+            if arg[0] not in storage.classes():
+                print("** class doesn't exist **")
+            elif len(arg) == 1 or arg[1] is None or arg[1] == "":
+                print("** instance id missing **")
+            else:
+                key = arg[0] + "." + arg[1]
+                if key in storage.all():
+                    print(storage.all()[key])
+                else:
+                    print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id
+        """
+        if arg is None or arg == "":
+            print("** class name missing **")
+        else:
+            arg = arg.split()
+            if arg[0] not in storage.classes():
+                print("** class doesn't exist **")
+            elif len(arg) == 1 or arg[1] is None or arg[1] == "":
+                print("** instance id missing **")
+            else:
+                key = arg[0] + "." + arg[1]
+                if key in storage.all():
+                    del storage.all()[key]
+                    storage.save()
+                else:
+                    print("** no instance found **")
+
+    def do_all(self, arg):
+        """Prints all string representation of all
+        instances based or not on the class name
+        """
+        if storage.all() is None or storage.all() == {}:
+            print("[]")
+        else:
+            print(storage.all())
+
+    def do_update(self, arg):
+        """Updates an instance based on the class
+        name and id by adding or updating attribute
+        """
 
 
 if __name__ == '__main__':
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,13 +25,21 @@ class FileStorage:
         with open(self.__file_path, "w") as file:
             json.dump(serialized, file)
 
+    @staticmethod
+    def classes():
+        """ Returns a dict of classes """
+        from models.base_model import BaseModel
+        list_classes = {
+            "BaseModel": BaseModel,
+        }
+        return list_classes
+
     def reload(self):
         """ Deserializes the JSON file to __objects """
         if exists(self.__file_path):
-            from models.base_model import BaseModel
             with open(FileStorage.__file_path) as file:
                 data = json.load(file)
             dummy = None
             for key, value in data.items():
-                dummy = BaseModel(**value)
+                dummy = self.classes()[key.split('.')[0]](**value)
                 FileStorage.__objects[key] = dummy
