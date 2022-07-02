4bbf9ae7f3ee2b14b22c7250780c792be00063cd
Nachop51 <nachoperalta0@gmail.com>
2022-07-02 16:02:26 +0000

    update method fixed

--- console.py
+++ console.py
@@ -155,7 +155,7 @@ class HBNBCommand(cmd.Cmd):
                     else:
                         a = args.split(',')[1].strip().split('"')
                         value = args.split(',')[2].strip().split('"')
-                        return f"{command} {classname} {id} {a[1:-1]} {value}"
+                        return f"{command} {classname} {id} {a} {value}"
                 return f"{command} {classname}"
         return arg
 
